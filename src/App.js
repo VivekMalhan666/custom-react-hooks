61e31223cee0fe1a2a537c4592474053f8ddc98c
fsociety <fsociety@root>
2021-10-16 01:14:58 +0530

    Initial Route Setup - react-router-dom - Router - Routes - Switches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
-  return <div className="app"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"></Route>
+        <Route path="/"></Route>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
