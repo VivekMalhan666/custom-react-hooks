82b8f1581789e26ccd6b962dd567f7811f8c2dd7
fsociety <fsociety@root>
2021-10-23 16:17:29 +0530

    useStateWithValidation - useStateWithValidation hook - a hook to validate a state at any instance - eg. input for password,etc

--- src/useStateWithValidation/StateWithValidationComponent.jsx
+++ src/useStateWithValidation/StateWithValidationComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import SideBar from '../components/SideBar';
 import useStateWithValidation from './useStateWithValidation';
 
 function StateWithValidationComponent() {
   const [username, setUsername, isValid] = useStateWithValidation(
-    name => name.length > 5,
+    (name) => name.length > 5,
     ''
   );
   return (
     <div>
+      <SideBar />
       <div>Valid: {isValid.toString()}</div>
       <input
         name="username"
