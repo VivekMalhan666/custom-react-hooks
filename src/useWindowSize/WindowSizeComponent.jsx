5e02ce5ee9dafb888ee92aa41391f9c9ce8ff90b
fsociety <fsociety@root>
2021-10-23 16:14:20 +0530

    useWindowSize - useWindowSize hook - get height and width of your screen - useEventListener is a dependency hook

--- src/useWindowSize/WindowSizeComponent.jsx
+++ src/useWindowSize/WindowSizeComponent.jsx
@@ -1,9 +1,11 @@
+import SideBar from '../components/SideBar';
 import useWindowSize from './useWindowSize';
 
 function WindowSizeComponent() {
   const { width, height } = useWindowSize();
   return (
     <div>
+      <SideBar />
       {width} x {height}
     </div>
   );
