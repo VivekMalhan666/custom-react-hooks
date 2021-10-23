b0958e5f9bb3b23f82f28c20cb323921379ba0fe
fsociety <fsociety@root>
2021-10-23 16:14:54 +0530

    useMediaQuery - useMediaQuery hook - helps use media queries of css in js - useEventListener is a dependency hook

--- src/useMediaQuery/MediaQueryComponent.jsx
+++ src/useMediaQuery/MediaQueryComponent.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import SideBar from '../components/SideBar';
 import useMediaQuery from './useMediaQuery';
 
 function MediaQueryComponent() {
   const isLarge = useMediaQuery('(min-width: 200px)');
 
-  return <div>Large: {isLarge.toString()}</div>;
+  return (
+    <div>
+      <SideBar />
+      Large: {isLarge.toString()}
+    </div>
+  );
 }
 
 export default MediaQueryComponent;
