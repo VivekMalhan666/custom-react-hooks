9effb3569ac80f385225197cea9e9b9126a6f3d4
fsociety <fsociety@root>
2021-10-23 15:46:58 +0530

    useToggle - useToggle hook - Toggle booleans - Make value a particular boolean - useState - prop-types

--- src/useToggle/ToggleComponent.jsx
+++ src/useToggle/ToggleComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SideBar from '../components/SideBar';
 import useToggle from './useToggle';
 
 function ToggleComponent() {
   const [value, toggleValue] = useToggle(false);
   return (
     <div>
+      <SideBar />
       <div>{value.toString()}</div>
       <button onClick={toggleValue}>ToggleValue</button>
       <button onClick={() => toggleValue(true)} type="button">
