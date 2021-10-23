2ed04e78696e0fb809f309a8908cc8c3ca746c7c
fsociety <fsociety@root>
2021-10-23 16:16:48 +0530

    useGeolocation - useGeolocation hook - get the location of user

--- src/useGeoLocation/GeoLocationComponent.jsx
+++ src/useGeoLocation/GeoLocationComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SideBar from '../components/SideBar';
 import useGeolocation from './useGeoLocation';
 
 function GeoLocationComponent() {
@@ -9,13 +10,14 @@ function GeoLocationComponent() {
   } = useGeolocation();
 
   return (
-    <>
+    <div>
+      <SideBar />
       <div>Loading:{loading.toString()}</div>
       <div>Error: {error?.message}</div>
       <div>
         {latitude} x {longitude}
       </div>
-    </>
+    </div>
   );
 }
 
